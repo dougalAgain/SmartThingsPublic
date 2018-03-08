2fd2f6d37d87a95c352f95c4d67853f2a0f87524
Edward Cann <edward.cann@gmail.com>
2018-03-08 17:42:38 +0000

    updated copyright date

--- devicetypes/dougalagain/eurotronic-spirit-trv.src/eurotronic-spirit-trv.groovy
+++ devicetypes/dougalagain/eurotronic-spirit-trv.src/eurotronic-spirit-trv.groovy
@@ -1,7 +1,7 @@
 /**
  *  Eurotronic Spirit TRV
  *
- *  Copyright 2016 Ed Cann
+ *  Copyright 2018 Ed Cann
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
